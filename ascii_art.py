6973d1fa6f9d9bf30397b9f7b5413e89c3b8c0a1
XDOwO <a28615781a@gmail.com>
2022-12-11 23:41:43 +0800

    Change variables' names to make it more readable.

--- ascii_art.py
+++ ascii_art.py
@@ -1,26 +1,21 @@
 from PIL import Image
 import numpy as np
 import time
-import os
-import sys
-x=95
-d="@QB#NgWM8RDHdOKq9$6khEPXwmeZaoS2yjufF]}{tx1zv7lciL/\\|?*>r^;:_\"~,'.-` "
-ss="\n"*x
+LENGTH=95
+GREY_MAP="@QB#NgWM8RDHdOKq9$6khEPXwmeZaoS2yjufF]}{tx1zv7lciL/\\|?*>r^;:_\"~,'.-` "
+ss="\n"*LENGTH
 def image_to_ascii_converter(name):
-    global d
     im =Image.open(name)
     im=im.convert("L")
-    im.thumbnail((im.size[0]/im.size[1]*x,x))
+    im.thumbnail((im.size[0]/im.size[1]*LENGTH,LENGTH))
     arr=np.array(im)/256*69
     arr=arr.astype(int)
-    s=[]
-    sss=""
+    im_in_ascii_arr=[]
+    im_in_ascii=""
     for i in arr:
-        t="".join(d[j] for j in i)
-        s.append(t)
-    for i in s:
-        sss+="\n"+i
-    # write
-    # os.system('cls')
-    print(sss)
+        t="".join(GREY_MAP[j] for j in i)
+        im_in_ascii_arr.append(t)
+    for i in im_in_ascii_arr:
+        im_in_ascii+="\n"+i
+    print(im_in_ascii)
     time.sleep(1/60-0.0002)
