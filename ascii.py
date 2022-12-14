e7f0afd64969c964ba171ef80e0e9739079e3a10
XDOwO <a28615781a@gmail.com>
2022-12-15 00:22:19 +0800

    Add file list and support for ascii.

--- ascii.py
+++ ascii.py
@@ -1,20 +1,23 @@
 from PIL import Image
 import numpy as np
 import time
-LENGTH=95
+LENGTH=325
 GREY_MAP="@QB#NgWM8RDHdOKq9$6khEPXwmeZaoS2yjufF]}{tx1zv7lciL/\\|?*>r^;:_\"~,'.-` "
 ss="\n"*LENGTH
-def image_to_ascii_converter(name):
-    im =Image.open(name)
+def image_to_ascii_converter(im):
     im=im.convert("L")
     im.thumbnail((im.size[0]/im.size[1]*LENGTH,LENGTH))
+    im.show()
     arr=np.array(im)/256*69
     arr=arr.astype(int)
     im_in_ascii_arr=[]
     im_in_ascii=""
     for i in arr:
-        t="".join(GREY_MAP[j] for j in i)
+        t="".join(GREY_MAP[j]*3 for j in i)
         im_in_ascii_arr.append(t)
     for i in im_in_ascii_arr:
         im_in_ascii+="\n"+i
-    print(im_in_ascii)
+    return im_in_ascii
+# a=image_to_ascii_converter(Image.open("test.jpg"))
+# print(a)
+# print(len(a))
--- control.py
+++ control.py
@@ -1,6 +1,31 @@
 from PyQt5 import QtWidgets,QtGui,QtCore
 from ui import Ui_MainWindow
 from PIL import ImageQt
+from os import listdir
+from os.path import isfile,join
+class FileList():
+    def __init__(self):
+        self.pointer=0
+        self.file_list=[]
+    def __init__(self,dir):
+        self.pointer=0
+        self.file_list = [f for f in listdir(dir) if isfile(join(dir, f)) and self.isImage(join(dir, f))]
+        self.file=self.file_list[self.pointer]
+    def isImage(self,file):
+        if ".jpg" in file or ".png" in file or ".jpeg" in file or ".gif" in file or ".bmp" in file:
+            return True
+        return False
+    def printf(self):
+        print(self.file_list[self.pointer])
+    def next(self):
+        self.pointer = (self.pointer+1) % len(self.file_list)
+        self.file=self.file_list[self.pointer]
+    def prev(self):
+        self.pointer = (self.pointer-1) % len(self.file_list)
+        self.file=self.file_list[self.pointer]
+    def find_file(self,s):
+        self.pointer = self.file_list.index(s)
+        self.file=self.file_list[self.pointer]
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -12,6 +37,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.setup()
     def setup(self):
+        self.ui.ascii_displayer.hide()
+        self.ui.exit_ascii_displayer_btn.hide()
+        self.ui.scrollArea.setWidgetResizable(True)
+        self.ui.imgLabel.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
         self.ui.actionOpen.triggered.connect(self.open_im)
         self.ui.actionOpen.setShortcut("Ctrl+O")
         self.ui.actionSave.triggered.connect(self.save_im)
@@ -24,23 +53,53 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.actionZoom_Out.setShortcut("Ctrl+-")
         self.ui.actionDefault_Size.triggered.connect(self.zoom_to_default)
         self.ui.actionDefault_Size.setShortcut("Ctrl+\\")
+        self.ui.actionAscii_Art.triggered.connect(self.show_ascii)
+        self.ui.file_tree.clicked.connect(self.file_clicked)
+        self.ui.next_img.clicked.connect(self.next_clicked)
+        self.ui.next_img.setShortcut("Right")
+        self.ui.prev_img.clicked.connect(self.prev_clicked)
+        self.ui.prev_img.setShortcut("Left")
+        self.ui.exit_ascii_displayer_btn.clicked.connect(self.exit_ascii)
+    def set_img(self):
+        
+        self.image=QtGui.QImage(self.dir+"/"+self.file_list.file)
+        if self.image.isNull():
+            QtWidgets.QMessageBox.information(self, "Image Viewer", "Cannot load %s." % self.file_list.file)
+            return
+        if self.ui.imgLabel.size().height() < self.image.size().height() or  self.ui.imgLabel.size().width() < self.image.size().width():
+            self.show_image=QtGui.QPixmap.fromImage(self.image).scaled(self.ui.imgLabel.size(),aspectRatioMode=QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+        else:
+            self.show_image=QtGui.QPixmap.fromImage(self.image)
+        self.ui.imgLabel.setPixmap(self.show_image)
+        self.ui.filename.setText(self.dir+'/'+self.file_list.file)
+        self.zoom_factor=1.0
+        self.zoom_to_default()
+        
+    def model_element_append(self):
+        self.model = QtGui.QStandardItemModel()
+        for file in self.file_list.file_list:
+            i = QtGui.QStandardItem(file)
+            self.model.appendRow(i)
+
+            i.setData(QtGui.QIcon(join(self.dir,'ico','image.png')),1)
     def open_im(self):
         fileName, fileType = QtWidgets.QFileDialog.getOpenFileName(self, 'Open Image','','Images (*.png *.jpeg *.jpg *.bmp *.gif)')
         if fileName:
             self.type=fileType
-            self.image=QtGui.QImage(fileName)
-            if self.ui.imgLabel.size().height() < self.image.size().height() or  self.self.ui.imgLabel.size().width() < self.image.size().width():
-                self.show_image=QtGui.QPixmap.fromImage(self.image).scaled(self.ui.imgLabel.size(),aspectRatioMode=QtCore.Qt.AspectRatioMode.KeepAspectRatio)
-            else:
-                self.show_image=QtGui.QPixmap.fromImage(self.image)
-            self.path=fileName
+            self.dir=fileName[:fileName.rfind('/')]
+            self.file_list=FileList(self.dir)
+            self.file_list.find_file(fileName[fileName.rindex('/')+1:])
+            self.set_img()
             if self.image.isNull():
-                QtWidgets.QMessageBox.information(self, "Image Viewer", "Cannot load %s." % fileName)
                 return
-            self.zoom_factor=1.0
-            
-            self.ui.imgLabel.setPixmap(self.show_image)
-            self.ui.filename.setText(fileName)
+            # self.model = QtWidgets.QFileSystemModel()
+            # self.model.setRootPath(self.dir)
+            # self.model.setFilter(QtCore.QDir.AllDirs | QtCore.QDir.AllEntries | QtCore.QDir.NoDotAndDotDot)
+            # self.model.setNameFilters(("*.png","*.jpeg","*.jpg","*.bmp","*.gif"))
+            # self.model.setNameFilterDisables(False)
+            self.model_element_append()
+            self.ui.file_tree.setModel(self.model)
+            # self.ui.file_tree.setRootIndex(self.model.index(self.dir))
             self.scaleFactor = 1.0
             self.ui.actionSave.setEnabled(True)
             self.ui.actionSave_as.setEnabled(True)
@@ -51,12 +110,12 @@ class MainWindow(QtWidgets.QMainWindow):
         if self.image is None or self.image.isNull():
             QtWidgets.QMessageBox.information(self, "Image Viewer", "An Error has occured when loading the Image.")
             return
-        self.image.save(self.path)
+        self.image.save(self.dir+'/'+self.file_list.file)
     def save_as_im(self):
         if self.image is None or self.image.isNull():
             QtWidgets.QMessageBox.information(self, "Image Viewer", "An Error has occured when loading the Image.")
             return
-        fileName, fileType = QtWidgets.QFileDialog.getSaveFileName(self, 'QFileDialog.getOpenFileName()','','Images (*.png *.jpeg *.jpg *.bmp *.gif)')
+        fileName, fileType = QtWidgets.QFileDialog.getSaveFileName(self, 'Save as','','Images (*.png *.jpeg *.jpg *.bmp *.gif)')
         self.image.save(fileName)
         # pass
     def zoom_in(self):
@@ -82,6 +141,29 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.actionZoom_In.setEnabled(True)
         self.ui.imgLabel.setPixmap(self.show_image)
         # pass
+    def show_ascii(self):
+        from ascii import image_to_ascii_converter
+        self.ui.ascii_displayer.show()
+        self.ui.exit_ascii_displayer_btn.show()
+        ascii = image_to_ascii_converter(ImageQt.fromqimage(self.image))
+        self.ui.ascii_displayer.setText(ascii)
+    def exit_ascii(self):
+        self.ui.ascii_displayer.hide()
+        self.ui.exit_ascii_displayer_btn.hide()
+    def file_clicked(self):
+        index=self.ui.file_tree.selectedIndexes()[0]
+        dir_or_img = self.model.itemFromIndex(index).text()
+        if ".jpg" in dir_or_img or ".png" in dir_or_img or ".jpeg" in dir_or_img or ".gif" in dir_or_img or ".bmp" in dir_or_img :
+            self.file_list.find_file(dir_or_img)
+            self.set_img()
+
+    def next_clicked(self):
+        self.file_list.next()
+        self.set_img()
+
+    def prev_clicked(self):
+        self.file_list.prev()
+        self.set_img()
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
